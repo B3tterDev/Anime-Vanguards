a37ce23c57ab479e38b6ea105959d45c0a369ff8
B3tterDev <nattawut.developer@gmail.com>
2025-04-07 20:40:12 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,3 @@
-repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(20)until getgenv().Joebiden
+-- repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(20)until getgenv().Joebiden
+getgenv().key = {86999,72574,30113,83553,79324,117113}
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
